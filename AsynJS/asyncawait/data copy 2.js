a19cf132f56de1a91eb03d1357e201c71c3f035d
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-08-06 13:02:12 +0530

    okie

---
+++ AsynJS/asyncawait/data copy 2.js
@@ -0,0 +1,32 @@
+let employees = [{ id: 101, name: "Rahul", sal: 45000 }, { id: 102, name: "Sonia", sal: 55000 }]
+
+let createEmployee = (emp) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let flag = true;
+            flag ? resolve("Inserted Successfully") : reject("Not Inserted")
+            employees.push(emp)
+        }, 4000)
+
+    })
+}
+
+let getEmployees = () => {
+    setTimeout(() => {
+        let rows = ""
+        employees.forEach((emp) => {
+            rows = rows + `<tr>
+                                <td>${emp.id}</td>
+                                <td>${emp.name}</td>
+                                <td>${emp.sal}</td>
+                         </tr>`
+        })
+        document.getElementById("tbody_Data").innerHTML = rows
+    }, 1000)
+}
+
+let dummy = () => {
+    createEmployee({ id: 103, name: "Priynka", sal: 65000 })
+    getEmployees()
+}
+dummy()
---
+++ AsynJS/asyncawait/data copy.js
@@ -0,0 +1,10 @@
+let employees = [{ id: 101, name: "Rahul", sal: 45000 }, { id: 102, name: "Sonia", sal: 55000 }]
+
+let createEmployee = () => { return new Promise() }
+
+let getEmployees = () => { }
+
+let dummy = async () => {
+    await createEmployee()
+    getEmployees()
+}
