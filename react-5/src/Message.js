480f0b3496032fa31c6dcd03048eeccc1e5ef2f1
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-08-10 10:20:36 +0530

    okie

--- react-5/src/Message.js
+++
@@ -1,8 +0,0 @@
-function Message() {
-
-    return <div>
-        <h2>Hello,GM</h2>
-        <h2>Hello,GN</h2>
-    </div>
-}
-export default Message
--- react-5/src/index.js
+++ react-5/src/index.js
@@ -1,6 +1,2 @@
-import App from './App'
 import ReactDOM from 'react-dom'
-ReactDOM.render(<App />, document.getElementById('rajnikanth'))
-//ReactDOM.render(<div><h1>Hello, Mr Rajni Kanth</h1><h2>Nayana Tara</h2></div>, document.getElementById('rajnikanth'))
-//ReactDOM.render("Hello,GM", document.getElementById('rajnikanth'))
-//ReactDOM.render(1,2)
+ReactDOM.render("Hello,GM", document.getElementById('root'));
